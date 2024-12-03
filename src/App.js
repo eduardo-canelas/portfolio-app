fbfe09080131d5302d0099299fc1f2033144f86c
Eduardo Canelas <edu.canelas.e@gmail.com>
2024-12-03 13:37:06 -0500

    Set up react app and initial customization

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio!</h1>
+            <p>This is a work in progress.</p>
+        </div>
+    );
+}
+
+export default App;
